31a9bbdb4377ef6d8295f722ae91b1785a01e213
m.botrugno <m.botrugno@studenti.uniba.it>
2025-01-03 18:47:44 +0100

    [FEAT] - Inserito before

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -81,7 +81,7 @@ for VAR in $(compgen -v | grep '^COMMIT_GROUPS_'); do
     EMOJI=${!VAR}                # Ottiene il valore della variabile (emoji o nome)
     
     # Ottieni i commit successivi all'ultimo commit con il testo [CHANGELOG]
-    COMMITS=$(git log $BRANCH --grep="^\\[${TYPE^^}\\]" --pretty=format:"- %s (%h)" --reverse --after="$LAST_COMMIT_HASH")
+    COMMITS=$(git log $BRANCH --grep="^\\[${TYPE^^}\\]" --pretty=format:"- %s (%h)" --reverse --before="$LAST_COMMIT_HASH")
     
     # Verifica se ci sono commit per quel gruppo
     if [ ! -z "$COMMITS" ]; then
