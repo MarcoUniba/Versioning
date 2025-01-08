6aa6e20dd26e1649d20f155e734dbe155accc055
m.botrugno <m.botrugno@studenti.uniba.it>
2025-01-08 19:20:34 +0100

    [FEAT][PMSBH-143] - Prova tag jira

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -91,17 +91,20 @@ for VAR in $(compgen -v | grep '^COMMIT_GROUPS_'); do
     if [ ! -z "$COMMITS" ]; then
         echo "## ${EMOJI}" >> "$CHANGELOG_FILE"   # Aggiunge la sezione del gruppo
         
-        # Modifica il formato del commit rimuovendo il gruppo
+        # Modifica il formato del commit rimuovendo solo il gruppo
         while IFS= read -r line; do
-            CLEAN_COMMIT=$(echo "$line" | sed -E 's/^\[[A-Z]+\](\[[A-Z0-9-]+\])? - //') # Rimuove il gruppo e l'eventuale tag Jira
-            
-            # Aggiungi il link al tag Jira se presente
+
+            # TODO: PROVARE
+            # Rimuove il tag del gruppo (es. [FIX])
+            CLEAN_COMMIT=$(echo "$line" | sed -E 's/^\[[A-Z]+\] //')
+
+            # Cerca il tag Jira e crea il link
             if [[ "$line" =~ \[([A-Z]+-[0-9]+)\] ]]; then
                 JIRA_TAG="${BASH_REMATCH[1]}"
                 LINK="[${JIRA_TAG}](${JIRA_URL}${JIRA_TAG})"
                 CLEAN_COMMIT=$(echo "$CLEAN_COMMIT" | sed "s/$JIRA_TAG/$LINK/")
             fi
-            
+
             echo "- $CLEAN_COMMIT" >> "$CHANGELOG_FILE"
         done <<< "$COMMITS"
         
