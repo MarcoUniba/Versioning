35e21e75cc518084ed7c00280d1700d07dbc7502
m.botrugno <m.botrugno@studenti.uniba.it>
2025-01-16 19:22:25 +0100

    [FEAT] - [PMSBH-143] Implementazione finale

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+WARNING='\033[1;93m'
+DANGER='\033[1;91m'
+NC='\033[0m' # No Color
 
 # Funzione per incrementare la versione basata sul tag
 increment_version() {
@@ -29,16 +32,16 @@ increment_version() {
 }
 
 # Verifica se il file di configurazione esiste
-if [ -f config.env ]; then
-    source config.env
+if [ -f config_changelog.env ]; then
+    source config_changelog.env
 else
-    echo "Errore: Il file config.env non esiste!"
+    echo "Errore: Il file config_changelog.env non esiste!"
     exit 1
 fi
 
 # Controlla se la variabile JIRA_URL è definita
 if [ -z "$JIRA_URL" ]; then
-    echo "Errore: La variabile JIRA_URL non è definita in config.env!"
+    echo "Errore: La variabile JIRA_URL non è definita in config_changelog.env!"
     exit 1
 fi
 
@@ -62,59 +65,58 @@ echo "# Changelog del $DATE (versione $NEW_VERSION)" > "$CHANGELOG_FILE"
 echo "" >> "$CHANGELOG_FILE"
 
 # Scarica i nuovi commit dal repository remoto
-git pull origin "$BRANCH"
+# git pull origin "$BRANCH"
 
 # Cerca l'ultimo commit con il numero di versione tra parentesi quadre
 LAST_VERSION_COMMIT=$(git log --grep="^\[${LAST_TAG}\]" --format="%H" -n 1)
 
 if [ -z "$LAST_VERSION_COMMIT" ]; then
-    echo "Nessun commit trovato per il tag ${LAST_TAG}."
-    exit 1
-fi
+    echo "${WARNING}[WARN] Nessun commit trovato per il tag ${LAST_TAG}.${NC}"
+    # exit 1
+else
+    # Ottieni la data dell'ultimo commit con il numero di versione
+    LAST_COMMIT_DATE=$(git log --format="%cI" -n 1 "$LAST_VERSION_COMMIT")
 
-# Ottieni la data dell'ultimo commit con il numero di versione
-LAST_COMMIT_DATE=$(git log --format="%cI" -n 1 "$LAST_VERSION_COMMIT")
-
-# Se non c'è nessun commit precedente, usa la data dell'ultimo tag
-if [ -z "$LAST_COMMIT_DATE" ]; then
-    LAST_COMMIT_DATE=$(git show -s --format=%cI "$LAST_TAG")
-fi
-
-# Ciclo per generare le sezioni del changelog basato sui gruppi di commit definiti nel file config.env
-for VAR in $(compgen -v | grep '^COMMIT_GROUPS_'); do
-    TYPE=${VAR#COMMIT_GROUPS_}   # Rimuove il prefisso "COMMIT_GROUPS_"
-    EMOJI=${!VAR}                # Ottiene il valore della variabile (emoji o nome)
-
-    # Ottieni i commit successivi alla data dell'ultimo commit con il numero di versione
-    COMMITS=$(git log $BRANCH --grep="^\[${TYPE^^}\\]" --pretty=format:"%s (%h)" --reverse --after="$LAST_COMMIT_DATE")
-
-    # Verifica se ci sono commit per quel gruppo
-    if [ ! -z "$COMMITS" ]; then
-        echo "## ${EMOJI}" >> "$CHANGELOG_FILE"   # Aggiunge la sezione del gruppo
-        while IFS= read -r line; do
-        echo "DEBUG: Processing commit: $line"
-            # Rimuove solo il tag del gruppo (es. [FEAT])
-            CLEAN_COMMIT=$(echo "$line" | sed -E 's|^\[[A-Z]+\] ||')
-            # Cerca il tag Jira e crea il link
-            if [[ "$line" =~ \[([A-Z]+-[0-9]+)\] ]]; then
-                JIRA_TAG="${BASH_REMATCH[1]}"
-                LINK="[$JIRA_TAG](${JIRA_URL}${JIRA_TAG})"
-                CLEAN_COMMIT=$(echo "$CLEAN_COMMIT" | sed -E "s|\[$JIRA_TAG\]|$LINK|")
-            fi
-
-            # Scrivi il commit nel changelog
-            echo "$CLEAN_COMMIT" >> "$CHANGELOG_FILE"
-        done <<< "$COMMITS"
-
-        # Aggiunge una riga vuota per separare gruppi
-        echo "" >> "$CHANGELOG_FILE"
+    # Se non c'è nessun commit precedente, usa la data dell'ultimo tag
+    if [ -z "$LAST_COMMIT_DATE" ]; then
+        LAST_COMMIT_DATE=$(git show -s --format=%cI "$LAST_TAG")
     fi
-done
 
-# Aggiungi il footer, se presente
-if [ -n "$FOOTER" ]; then
-    echo "" >> "$CHANGELOG_FILE"
-    echo "# $FOOTER" >> "$CHANGELOG_FILE"
+    # Ciclo per generare le sezioni del changelog basato sui gruppi di commit definiti nel file config_changelog.env
+    for VAR in $(compgen -v | grep '^COMMIT_GROUPS_'); do
+        TYPE=${VAR#COMMIT_GROUPS_}   # Rimuove il prefisso "COMMIT_GROUPS_"
+        EMOJI=${!VAR}                # Ottiene il valore della variabile (emoji o nome)
+
+        # Ottieni i commit successivi alla data dell'ultimo commit con il numero di versione
+        COMMITS=$(git log $BRANCH --grep="^\[${TYPE^^}\\]" --pretty=format:"%s (%h)" --reverse --after="$LAST_COMMIT_DATE")
+
+        # Verifica se ci sono commit per quel gruppo
+        if [ ! -z "$COMMITS" ]; then
+            echo "## ${EMOJI}" >> "$CHANGELOG_FILE"   # Aggiunge la sezione del gruppo
+            while IFS= read -r line; do
+                # echo "DEBUG: Processing commit: $line"
+                # Rimuove solo il tag del gruppo (es. [FEAT])
+                CLEAN_COMMIT=$(echo "$line" | sed -E 's|^\[[A-Z]+\] ||')
+                # Cerca il tag Jira e crea il link
+                if [[ "$line" =~ \[([A-Z]+-[0-9]+)\] ]]; then
+                    JIRA_TAG="${BASH_REMATCH[1]}"
+                    LINK="[$JIRA_TAG](${JIRA_URL}${JIRA_TAG})"
+                    CLEAN_COMMIT=$(echo "$CLEAN_COMMIT" | sed -E "s|\[$JIRA_TAG\]|$LINK|")
+                fi
+
+                # Scrivi il commit nel changelog
+                echo "$CLEAN_COMMIT" >> "$CHANGELOG_FILE"
+            done <<< "$COMMITS"
+
+            # Aggiunge una riga vuota per separare gruppi
+            echo "" >> "$CHANGELOG_FILE"
+        fi
+    done
+    # Aggiungi il footer, se presente
+    if [ -n "$FOOTER" ]; then
+        echo "" >> "$CHANGELOG_FILE"
+        echo "# $FOOTER" >> "$CHANGELOG_FILE"
+    fi
 fi
 
 # Aggiungi il changelog e aggiorna il repository con il nuovo tag
